9349719d1f77cbf879cc6eba4b9647991decc92a
Amine Laaroussi <amine.laaroussi05@gmail.com>
2022-06-21 09:51:45 +0200

    feat: Ajout des méthodes create update delete :sparkles:
    executeUpdate()

--- src/main/java/demo/demoJDBC/dao/sql/UtilisateurDAO.java
+++ src/main/java/demo/demoJDBC/dao/sql/UtilisateurDAO.java
@@ -124,16 +124,123 @@ public class UtilisateurDAO implements UserDao {
 
     @Override
     public void create(Utilisateur obj) {
+        // Pour communiquer j'ai besoin de 2 infos:
+        Connection connection = null;
+        PreparedStatement ps = null;
 
+        // Ouvrir ma connexion
+        try {
+            connection = ConfigMySQL.getInstance().getConnection();
+            // Préparer ma requête
+            ps = connection.prepareStatement("INSERT INTO UTILISATEUR (id, nom, prenom, age) VALUES (?,?,?,?)");
+            ps.setLong(1, obj.getId());
+            ps.setString(2,obj.getNom());
+            ps.setString(3, obj.getPrenom());
+            ps.setInt(4,obj.getAge());
+
+            /*ps = connection.prepareStatement("INSERT INTO UTILISATEUR (nom, prenom, age) VALUES (?,?,?)", PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(2,obj.getNom());
+            ps.setString(3, obj.getPrenom());
+            ps.setInt(4,obj.getAge());*/
+            // Exécuter la requête avec mon interprète
+            int rows = ps.executeUpdate();
+            if(rows != 1){
+                    throw new Exception("problème à la création de l'utilisateur:" + obj);
+
+            } /* Dans le cas où la clé est généré par ma base de donnée
+            else{
+                ResultSet keys =  ps.getGeneratedKeys();
+                if(keys.next()){
+                    Long id = keys.getLong(1);
+                    obj.setId(id);
+                }
+            }*/
+            // parcours ma liste de résultat avec une boucle
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally{
+            // Fermer les instances de communications
+            ConfigMySQL.getInstance().closeResultEtPS(null, ps);
+            ConfigMySQL.getInstance().seDeconnecter();
+        }
     }
 
     @Override
     public void update(Utilisateur obj) {
+// Pour communiquer j'ai besoin de 2 infos:
+        Connection connection = null;
+        PreparedStatement ps = null;
 
+        // Ouvrir ma connexion
+        try {
+            connection = ConfigMySQL.getInstance().getConnection();
+            // Préparer ma requête
+            ps = connection.prepareStatement("UPDATE Utilisateur SET nom = ?, prenom = ?, age = ? WHERE id = ?");
+            ps.setLong(4, obj.getId());
+            ps.setString(1,obj.getNom());
+            ps.setString(2, obj.getPrenom());
+            ps.setInt(3,obj.getAge());
+
+
+            // Exécuter la requête avec mon interprète
+            int rows = ps.executeUpdate();
+            if(rows != 1){
+                throw new Exception("problème à la modification de l'utilisateur:" + obj);
+
+            }
+            // parcours ma liste de résultat avec une boucle
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally{
+            // Fermer les instances de communications
+            ConfigMySQL.getInstance().closeResultEtPS(null, ps);
+            ConfigMySQL.getInstance().seDeconnecter();
+        }
     }
 
     @Override
     public void delete(Long id) {
+// Pour communiquer j'ai besoin de 3 infos:
+        Connection connection = null;
+        PreparedStatement ps = null;
 
+        // Ouvrir ma connexion
+        try {
+            connection = ConfigMySQL.getInstance().getConnection();
+            // Préparer ma requête
+            ps = connection.prepareStatement("DELETE FROM UTILISATEUR WHERE id = ?");
+            ps.setLong(1, id);
+
+            /*ps = connection.prepareStatement("INSERT INTO UTILISATEUR (nom, prenom, age) VALUES (?,?,?)", PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(2,obj.getNom());
+            ps.setString(3, obj.getPrenom());
+            ps.setInt(4,obj.getAge());*/
+            // Exécuter la requête avec mon interprète
+            int rows = ps.executeUpdate();
+            if(rows != 1){
+                throw new Exception("problème à la suppression de l'utilisateur à l'id:" + id);
+
+            } /* Dans le cas où la clé est généré par ma base de donnée
+            else{
+                ResultSet keys =  ps.getGeneratedKeys();
+                if(keys.next()){
+                    Long id = keys.getLong(1);
+                    obj.setId(id);
+                }
+            }*/
+            // parcours ma liste de résultat avec une boucle
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally{
+            // Fermer les instances de communications
+            ConfigMySQL.getInstance().closeResultEtPS(null, ps);
+            ConfigMySQL.getInstance().seDeconnecter();
+        }
     }
 }
